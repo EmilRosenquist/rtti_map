602bab9593fe732ce9a554664593a84d3a13664e
Emil Rosenquist <emil.rosenquist@gmail.com>
2022-08-15 19:37:34 +0200

    Initial implementation of rtti_map with respective tests.

--- rtti_map/rtti_map.hxx
+++ rtti_map/rtti_map.hxx
@@ -1,9 +1,52 @@
 #pragma once
 
+#include <any>
+#include <typeinfo>
+#include <unordered_map>
+
 namespace rtti
 {
   struct map
   {
+    // Capacity
+    [[nodiscard]] auto empty() const noexcept { return map_.empty(); }
+    [[nodiscard]] auto size() const noexcept { return map_.size(); }
 
+    // Modifiers
+    auto clear() noexcept { map_.clear(); }
+    template <typename T>
+    auto insert(T&& t)
+    {
+        return map_.insert({ typeid(T).hash_code(), t }).second;
+    }
+    template <typename T>
+    auto insert_or_assign(T&& t)
+    {
+        return map_.insert_or_assign(typeid(T).hash_code(), t).second;
+    }
+    template <typename T, typename... Args>
+    auto emplace(Args&&... args)
+    {
+        auto t = std::any{};
+        t.emplace<T>(args...);
+        return map_.emplace(typeid(T).hash_code(), std::move(t)).second;
+    }
+    template <typename T>
+    auto erase() noexcept { return map_.erase(typeid(T).hash_code()); }
+
+    // Lookup
+    template <typename T>
+    [[nodiscard]] T& at() { return std::any_cast<T&>(map_.at(typeid(T).hash_code())); }
+    template <typename T>
+    [[nodiscard]] const T& at() const { return std::any_cast<const T&>(map_.at(typeid(T).hash_code())); }
+    template <typename T>
+    [[nodiscard]] auto count() const { return map_.count(typeid(T).hash_code()); }
+    template <typename T>
+    [[nodiscard]] auto contains() const { return map_.contains(typeid(T).hash_code()); }
+
+    // Hash policy
+    auto reserve(std::size_t max) { map_.reserve(max); }
+  private:
+    std::unordered_map<std::size_t, std::any> map_;
   };
 }
--- tests/basics/driver.cxx
+++ tests/basics/driver.cxx
@@ -9,8 +9,73 @@
 
 int main ()
 {
+  // Capacity tests
   {
-    const auto map = std::make_unique<rtti::map>();
-    assert (map != nullptr);
+    auto map = rtti::map{};
+
+    assert(map.empty());
+    assert(map.size() == 0);
+
+    map.insert<int>(1);
+    assert(!map.empty());
+    assert(map.size() == 1);
+  }
+
+  // Modifiers tests
+  {
+    auto map = rtti::map{};
+
+    const auto insertionResult = map.insert<int>(1);
+    const auto insertionResult2 = map.insert<int>(2);
+
+    assert(map.at<int>() == 1);
+    assert(insertionResult);
+    assert(!insertionResult2);
+
+    const auto insertionResult3 = map.insert_or_assign<double>(3);
+    const auto insertionResult4 = map.insert_or_assign<int>(4);
+
+    assert(map.at<double>() == 3);
+    assert(map.at<int>() == 4);
+    assert(insertionResult3);
+    assert(!insertionResult4);
+
+    struct A{ int b, c; };
+    const auto emplaceResult1 = map.emplace<A>(5, 5);
+    const auto emplaceResult2 = map.emplace<int>(6);
+
+    assert(map.at<A>().b == 5 && map.at<A>().c == 5);
+    assert(map.at<int>() == 4);
+    assert(emplaceResult1);
+    assert(!emplaceResult2);
+
+    const auto eraseResult1 = map.erase<int>();
+    const auto eraseResult2 = map.erase<float>();
+
+    assert(eraseResult1 == 1);
+    assert(eraseResult2 == 0);
+
+    map.clear();
+    assert(map.empty());
+  }
+
+  // Lookup tests
+  {
+    auto map = rtti::map{};
+
+    // Empty initialized
+    assert(map.count<int>() == 0);
+    assert(!map.contains<int>());
+
+    map.insert<int>(1);
+
+    assert(map.count<int>() == 1);
+    assert(map.contains<int>());
+    assert(map.at<int>() == 1);
+
+    auto& elementReference = map.at<int>();
+    elementReference = 2;
+
+    assert(map.at<int>() == 2);
   }
 }
