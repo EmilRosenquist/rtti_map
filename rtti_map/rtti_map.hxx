749c6ac3c2c9cccdbb0f33bf76442df444cb089b
Emil Rosenquist <emil.rosenquist@gmail.com>
2022-08-18 17:09:48 +0200

    simplify code

--- rtti_map/rtti_map.hxx
+++ rtti_map/rtti_map.hxx
@@ -12,6 +12,9 @@ namespace rtti
 {
   namespace internal
   {
+    template <typename T>
+    concept non_ref_t = !std::is_reference_v<T>;
+
     template <typename std_container>
     struct container
     {
@@ -21,25 +24,25 @@ namespace rtti
 
       // Modifiers
       auto clear() noexcept { map_.clear(); }
-      template <typename T>
+      template <non_ref_t T>
       auto erase() noexcept { return map_.erase(typeid(T).hash_code()); }
 
       // Lookup
-      template <typename T>
+      template <non_ref_t T>
       [[nodiscard]] auto count() const { return map_.count(typeid(T).hash_code()); }
-      template <typename T>
+      template <non_ref_t T>
       [[nodiscard]] auto contains() const { return map_.contains(typeid(T).hash_code()); }
-      template <typename T> requires (!std::is_reference_v<T>)
+      template <non_ref_t T>
       [[nodiscard]] auto equal_range()
       {
         const auto range = map_.equal_range(typeid(T).hash_code());
-        return equal_range_internal<T>(range.first, range.second);
+        return create_range<T>(range.first, range.second);
       }
-      template <typename T> requires (!std::is_reference_v<T>)
+      template <non_ref_t T>
       [[nodiscard]] auto equal_range() const
       {
         const auto range = map_.equal_range(typeid(T).hash_code());
-        return equal_range_internal<std::add_const_t<T>>(range.first, range.second);
+        return create_range<std::add_const_t<T>>(range.first, range.second);
       }
 
       // Hash policy
@@ -48,7 +51,7 @@ namespace rtti
     protected:
       template <typename T>
       auto insert_internal(T&& t) { return map_.insert({ typeid(T).hash_code(), t }); }
-      template <typename T, typename... Args>
+      template <non_ref_t T, typename... Args>
       auto emplace_internal(Args&&... args)
       {
         auto t = std::any{};
@@ -58,22 +61,10 @@ namespace rtti
       std_container map_;
 
     private:
-      template <typename T>
-      [[nodiscard]] auto equal_range_internal(auto begin, auto end) const
-      {
-        if constexpr (std::is_object_v<T> || std::is_function_v<T>)
-        {
-          return create_range<T, std::reference_wrapper<T>>(begin, end);
-        }
-        else
-        {
-          return create_range<T>(begin, end);
-        }
-      }
-      template <typename T, typename reference_type = T>
+      template <non_ref_t T>
       [[nodiscard]] auto create_range(auto begin, auto end) const
       {
-        auto referenceRange = std::vector<reference_type>{};
+        auto referenceRange = std::vector<std::reference_wrapper<T>>{};
         referenceRange.reserve(std::distance(begin, end));
         std::transform(begin, end, std::back_inserter(referenceRange), [](auto& match)
         {
@@ -87,26 +78,26 @@ namespace rtti
   struct map : internal::container<std::unordered_map<std::size_t, std::any>>
   {
     // Modifiers
-    template <typename T>
+    template <internal::non_ref_t T>
     auto insert(T&& t) { return insert_internal(t).second; }
-    template <typename T>
+    template <internal::non_ref_t T>
     auto insert_or_assign(T&& t) { return map_.insert_or_assign(typeid(T).hash_code(), t).second; }
-    template <typename T, typename... Args>
+    template <internal::non_ref_t T, typename... Args>
     auto emplace(Args&&... args) { return emplace_internal<T>(args...).second; }
 
     // Lookup
-    template <typename T>
+    template <internal::non_ref_t T>
     [[nodiscard]] T& at() { return std::any_cast<T&>(map_.at(typeid(T).hash_code())); }
-    template <typename T>
+    template <internal::non_ref_t T>
     [[nodiscard]] const T& at() const { return std::any_cast<const T&>(map_.at(typeid(T).hash_code())); }
   };
 
   struct multimap : internal::container<std::unordered_multimap<std::size_t, std::any>>
   {
     // Modifiers
-    template <typename T>
+    template <internal::non_ref_t T>
     auto insert(T&& t) { insert_internal(t); }
-    template <typename T, typename... Args>
+    template <internal::non_ref_t T, typename... Args>
     auto emplace(Args&&... args) { emplace_internal<T>(args...); }
   };
 }
